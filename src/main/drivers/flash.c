98f7f5e00ec1853dde2187502c11fad260c9b37a
Martin Luessi <mluessi@gmail.com>
2022-08-11 07:59:46 -0700

    Flash: remove FW partition on BrainFPV targets

--- src/main/drivers/flash.c
+++ src/main/drivers/flash.c
@@ -358,7 +358,7 @@ static void flashConfigurePartitions(void)
         endSector = badBlockPartition->startSector - 1;
     }
 
-#if defined(FIRMWARE_SIZE)
+#if defined(FIRMWARE_SIZE) && !defined(BRAINFPV)
     const uint32_t firmwareSize = (FIRMWARE_SIZE * 1024);
     flashSector_t firmwareSectors = (firmwareSize / flashGeometry->sectorSize);
 
