a85adcc2cf711ef590aad93b1726b432c2fd4d70
Martin Luessi <mluessi@gmail.com>
2023-01-16 14:40:46 -0800

    BrainFPV OSD: fix arming logo when disarmed early

--- src/main/osd/osd.c
+++ src/main/osd/osd.c
@@ -1525,6 +1525,10 @@ bool brainFPVOsdUpdate(timeUs_t currentTimeUs)
         showVisualBeeper = true;
     }
 
+    if (!ARMING_FLAG(ARMED) && osdShowArming) {
+        osdShowArming = false;
+    }
+
     osdProcessStats1(currentTimeUs);
     osdProcessStats2(currentTimeUs);
     osdProcessStats3(currentTimeUs);
