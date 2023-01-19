d2ac28f2400d5c1cb710f9ed4ea026c6aad53c10
Martin Luessi <mluessi@gmail.com>
2023-01-18 16:16:38 -0800

    RADIX2HD: cleanup

--- src/main/target/RADIX2HD/target.mk
+++ src/main/target/RADIX2HD/target.mk
@@ -4,6 +4,8 @@ H750xB_TARGETS += $(TARGET)
 HSE_VALUE    = 16000000
 START_ADDRESS = 0x000000
 
+TARGET_FLASH_SIZE = 1024
+
 FEATURES       += VCP ONBOARDFLASH SDCARD SDCARD_SDIO CHIBIOS BRAINFPV BRAINFPV_BL
 
 TARGET_SRC += \
@@ -11,8 +13,4 @@ TARGET_SRC += \
               drivers/accgyro/accgyro_spi_bmi270.c \
               $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
               drivers/barometer/barometer_dps310.c \
-              drivers/compass/compass_hmc5883l.c \
-              drivers/compass/compass_qmc5883l.c \
-              drivers/compass/compass_lis3mdl.c \
-              drivers/compass/compass_ak8963.c \
-              drivers/compass/compass_ak8975.c \
+              $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c)))
