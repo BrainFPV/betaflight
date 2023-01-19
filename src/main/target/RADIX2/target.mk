ecbc210cde66db38d4fe48f1cd467702d48c68f1
Martin Luessi <mluessi@gmail.com>
2023-01-18 16:16:19 -0800

    RADIX2: cleanup make file

--- src/main/target/RADIX2/target.mk
+++ src/main/target/RADIX2/target.mk
@@ -4,9 +4,9 @@ H750xB_TARGETS += $(TARGET)
 HSE_VALUE    = 16000000
 START_ADDRESS = 0x000000
 
-FEATURES       += VCP ONBOARDFLASH CHIBIOS BRAINFPV SPECTROGRAPH BRAINFPV_BL
-
 TARGET_FLASH_SIZE = 1024
+
+FEATURES       += VCP ONBOARDFLASH CHIBIOS BRAINFPV SPECTROGRAPH BRAINFPV_BL
 
 TARGET_SRC += \
               drivers/accgyro/accgyro_mpu.c \
@@ -14,4 +14,4 @@ TARGET_SRC += \
               $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
               drivers/barometer/barometer_bmp388.c \
               drivers/barometer/barometer_2smpb_02b.c \
-              $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+              $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c)))
