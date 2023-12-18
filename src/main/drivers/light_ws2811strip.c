4f175438e1705657ea10a38986467e243b8ea3f9
Martin Luessi <mluessi@gmail.com>
2023-12-18 13:56:13 -0800

    BrainFPV: Fix LED strip DMA RAM

--- src/main/drivers/light_ws2811strip.c
+++ src/main/drivers/light_ws2811strip.c
@@ -60,7 +60,12 @@
 #define WS2811_DMA_BUF_CACHE_ALIGN_BYTES  ((WS2811_DMA_BUF_BYTES + 0x20) & ~0x1f)
 // Size of array to create a cache aligned buffer
 #define WS2811_DMA_BUF_CACHE_ALIGN_LENGTH (WS2811_DMA_BUF_CACHE_ALIGN_BYTES / sizeof(uint32_t))
-DMA_RW_AXI __attribute__((aligned(32))) uint32_t ledStripDMABuffer[WS2811_DMA_BUF_CACHE_ALIGN_LENGTH];
+#ifdef BRAINFPV
+#define LED_RAM DMA_RAM
+#else
+#define LED_RAM DMA_RW_AXI
+#endif
+LED_RAM __attribute__((aligned(32))) uint32_t ledStripDMABuffer[WS2811_DMA_BUF_CACHE_ALIGN_LENGTH];
 #else
 #if defined(STM32F7)
 FAST_DATA_ZERO_INIT uint32_t ledStripDMABuffer[WS2811_DMA_BUFFER_SIZE];
